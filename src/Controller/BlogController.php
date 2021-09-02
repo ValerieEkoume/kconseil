7e62b9b1e91d50d08f6d81656c10d6bc02330c5c
ValerieEkoume <valerie_ekoume@hotmail.com>
2021-09-02 15:40:09 +0200

    new Blog create

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Blog;
 use App\Repository\BlogRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blogs", name="app_blog")
+     * @Route("/blogs", name="app_blogs", methods="GET")
      */
     public function index(BlogRepository $blogRepository): Response
     {
@@ -20,7 +24,36 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blogs/{id<[0-9]+>}", name="app_blogs_show")
+     * @Route("/blogs/create", name="app_blogs_create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em):Response
+    {
+        $blog = new Blog;
+
+        $form = $this->createFormBuilder($blog)
+            ->add('title', TextType::class)
+            ->add('article', TextareaType::class)
+            ->add('prenom', TextType::class)
+            ->add('nom', TextType::class)
+            ->getForm()
+            ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($blog);
+            $em->flush();
+
+            return $this->redirectToRoute('app_blogs');
+        }
+
+        return $this->render('blogs/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/blogs/{id<[0-9]+>}", name="app_blogs_show", methods="GET")
      */
     public function show(Blog $blog) : Response
     {
