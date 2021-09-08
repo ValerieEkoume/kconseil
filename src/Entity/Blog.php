d8f005d8ce6da7ecfb6833715c763a48ada86de2
ValerieEkoume <valerie_ekoume@hotmail.com>
2021-09-08 16:42:10 +0200

    Add VichUploaderbundle

--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -6,10 +6,13 @@ use App\Entity\Traits\Timestampable;
 use App\Repository\BlogRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=BlogRepository::class)
  * @ORM\Table(name="blogs")
+ * @Vich\Uploadable
  * @ORM\HasLifecycleCallbacks
  */
 class Blog
@@ -47,6 +50,16 @@ class Blog
      * @Assert\NotBlank(message="N'oublie pas ton nom aussi")
      */
     private $nom;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="blog_image", fileNameProperty="imageName" )
+     *
+     * @var File|null
+     */
+    private $imageFile;
+
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -106,6 +119,25 @@ class Blog
         $this->nom = $nom;
 
         return $this;
+    }
+
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable);
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getImageName(): ?string
