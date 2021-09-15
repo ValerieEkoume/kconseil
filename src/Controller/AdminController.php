f52de4d92a86cffe858afaf21929fb9bd498b648
ValerieEkoume <valerie_ekoume@hotmail.com>
2021-09-15 09:53:11 +0200

    Création d'un controller Admin pour réorganisation de l'epace admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TarifsController extends AbstractController
+class AdminController extends AbstractController
 {
     /**
-     * @Route("/tarifs", name="app_tarifs")
+     * @Route("/admin", name="app_admin")
      */
     public function index(): Response
     {
-        return $this->render('tarifs/index.html.twig');
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
     }
 }
