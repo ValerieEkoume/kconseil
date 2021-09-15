83222e689c842a7b69250bf6d49d078d69f880ef
ValerieEkoume <valerie_ekoume@hotmail.com>
2021-09-15 11:50:02 +0200

    Add new entity Tarifs

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,103 @@
 
 namespace App\Controller;
 
+use App\Entity\Blog;
+use App\Form\BlogType;
+use App\Repository\BlogRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     /**
+     * @var BlogRepository
+     */
+    private $repository;
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(BlogRepository $repository, EntityManagerInterface $em)
+    {
+
+        $this->repository=$repository;
+        $this->em=$em;
+    }
+
+    /**
      * @Route("/admin", name="app_admin")
      */
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        $blogs = $this->repository->findAll();
+        return $this->render('admin/index.html.twig', compact('blogs'));
+    }
+
+    /**
+     * @Route("/blogs/create", name="app_blogs_create")
+     */
+    public function create(Request $request)
+    {
+        $blog = new Blog();
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($blog);
+            $this->em->flush();
+            $this->addFlash('success', 'Article créée avec succès');
+            return $this->redirectToRoute('app_admin');
+        }
+        return $this->render('blogs/create.html.twig', [
+            'blog' => $blog,
+            'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/blogs/{id<[0-9]+>}/edit", name="app_blogs_edit", methods= {"GET", "POST"})
+     * @param Blog $blog
+     * @return \Symfony\component\HttpFoundation\Response
+     */
+    public function edit(Blog $blog, Request $request)
+    {
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            $this->addFlash('success', 'Leçon modifiée avec succès');
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->render('blogs/edit.html.twig', [
+            'blog' => $blog,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     *  @Route("/admin/couts/delete/{id}", name="app_blogs_delete", methods="DELETE")
+     * @param Blog $blog
+     * @return \Symfony\component\HttpFoundation\Response
+     */
+    public function delete(Blog $blog, Request $request)
+    {
+        if ($this->isCsrfTokenValid('delete' . $blog->getId(), $request->get('token'))) {
+            $this->em->remove($blog);
+            $this->em->flush();
+            $this->addFlash('success', 'Leçon supprimée avec succès');
+
+        }
+        return $this->redirectToRoute('app_admin');
+
+
+    }
+
+
 }
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,11 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Blog;
-use App\Form\BlogType;
 use App\Repository\BlogRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,32 +20,6 @@ class BlogController extends AbstractController
         return $this->render('blogs/index.html.twig', compact('blogs'));
     }
 
-    /**
-     * @Route("/blogs/create", name="app_blogs_create", methods={"GET", "POST"})
-     */
-    public function create(Request $request, EntityManagerInterface $em):Response
-    {
-        $blog = new Blog;
-
-        $form = $this->createForm(BlogType::class, $blog)
-
-            ;
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($blog);
-            $em->flush();
-
-            $this->addFlash('success', 'L\'article de blog a bien été créé');
-
-            return $this->redirectToRoute('app_blogs');
-        }
-
-        return $this->render('blogs/create.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
 
     /**
      * @Route("/blogs/{id<[0-9]+>}", name="app_blogs_show", methods="GET")
@@ -56,46 +27,6 @@ class BlogController extends AbstractController
     public function show(Blog $blog) : Response
     {
         return $this->render('blogs/show.html.twig', compact('blog'));
-    }
-
-
-    /**
-     * @Route("/blogs/{id<[0-9]+>}/edit", name="app_blogs_edit", methods= {"GET", "POST"})
-     */
-    public function edit(Request $request, EntityManagerInterface $em, Blog $blog):Response
-    {
-        $form = $this->createForm(BlogType::class, $blog)
-          ;
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->flush();
-
-            $this->addFlash('success', 'Article modifié avec succès');
-
-            return $this->redirectToRoute('app_blogs');
-        }
-
-            return $this->render('blogs/edit.html.twig', [
-                'blog'=> $blog,
-                'form' => $form->createView()
-
-        ]);
-        }
-
-    /**
-     * @Route("/blogs/{id<[0-9]+>}/delete", name="app_blogs_delete", methods= {"GET"})
-     */
-    public function delete(Request $request, EntityManagerInterface $em, Blog $blog):Response
-    {
-
-        $em->remove($blog);
-        $em->flush();
-
-        $this->addFlash('info', 'Article  supprimé avec succès');
-
-        return $this->redirectToRoute('app_blogs');
     }
 
 
