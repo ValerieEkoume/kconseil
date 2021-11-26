f3b69cc333ce4f0be5521eee6a71e9cdb92cb72e
ValerieEkoume <valerie_ekoume@hotmail.com>
2021-11-26 20:55:11 +0100

    Add register form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegistrationType;
 use App\Repository\UserRepository;
 use App\Security\LoginFormAuthenticator;
 use App\services\Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -18,6 +20,7 @@ class SecurityController extends AbstractController
      * @var UserRepository
      */
     private UserRepository $userRepository;
+    private $passwordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder,
                                 UserRepository $userRepository)
@@ -25,6 +28,31 @@ class SecurityController extends AbstractController
         $this->passwordEncoder = $passwordEncoder;
         $this->userRepository = $userRepository;
     }
+
+    /**
+     * @Route("/inscription", name="app_register")
+     * @param Request $request
+     * @return Response
+     */
+    public function register(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $this->passwordEncoder->encodePassword($user, $form->get("password")->getData())
+            );
+        }
+
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView()
+
+        ]);
+
+    }
+
 
     /**
      * @Route("/kimtim", name="app_login")
