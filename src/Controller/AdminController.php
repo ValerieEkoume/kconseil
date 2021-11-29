d7e133d27b12d769e6f395404e4a8c723280e4b2
ValerieEkoume <valerie_ekoume@hotmail.com>
2021-11-29 15:21:25 +0100

    Fixing the registration security login

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -37,8 +37,8 @@ class AdminController extends AbstractController
 
 
     /**
-     * @Route("/admin", name="app_admin")
-     *
+     * @Route("/kimtim-admin", name="app_admin")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(): Response
     {
@@ -48,7 +48,7 @@ class AdminController extends AbstractController
     }
     /**
      * @Route("/admin-blog", name="app_admin_blog")
-     *
+     * @IsGranted("ROLE_ADMIN")
      */
     public function blog(): Response
     {
@@ -58,7 +58,7 @@ class AdminController extends AbstractController
 
     /**
      * @Route("/blogs/create", name="app_blogs_create")
-     *
+     * @IsGranted("ROLE_ADMIN")
      */
     public function create(Request $request)
     {
@@ -80,7 +80,7 @@ class AdminController extends AbstractController
 
     /**
      * @Route("/blogs/{id<[0-9]+>}/edit", name="app_blogs_edit", methods= {"GET", "POST"})
-     *
+     * @IsGranted("ROLE_ADMIN")
      * @param Blog $blog
      * @return \Symfony\component\HttpFoundation\Response
      */
